49663d6dffd0bc711f894f12b1bcf66cd0dae2ec
Raviteja Giduturi (from Dev Box) <rgiduturi@microsoft.com>
2025-04-15 23:24:05 +0530

    add sample flight booking plugin

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -26,21 +26,58 @@ var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
 
 var kernel = builder.Build();
+
 var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+kernel.Plugins.AddFromType<FlightBookingPlugin>("FlightBooking");
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
 
-string prompt = """
-    You are a helpful travel guide. 
-    I'm visiting {{$city}}. {{$background}}. What are some activities I should do today?
-    """;
-string city = "India";
-string background = "I really enjoy adventurous and new places.";
 
-// Create the kernel function from the prompt
-var activitiesFunction = kernel.CreateFunctionFromPrompt(prompt);
+// string prompt = """
+//     You are a helpful travel guide. 
+//     I'm visiting {{$city}}. {{$background}}. What are some activities I should do today?
+//     """;
+// string city = "India";
+// string background = "I really enjoy adventurous and new places.";
 
-// Create the kernel arguments
-var arguments = new KernelArguments { ["city"] = city, ["background"] = background };
+// // Create the kernel function from the prompt
+// var activitiesFunction = kernel.CreateFunctionFromPrompt(prompt);
 
-// InvokeAsync on the kernel object
-var result = await kernel.InvokeAsync(activitiesFunction, arguments);
-Console.WriteLine(result);
+// // Create the kernel arguments
+// var arguments = new KernelArguments { ["city"] = city, ["background"] = background };
+
+// // InvokeAsync on the kernel object
+// var result = await kernel.InvokeAsync(activitiesFunction, arguments);
+// Console.WriteLine(result);
+
+var history = new ChatHistory();
+history.AddSystemMessage("The year is 2025 and the current month is January");
+
+GetInput();
+await GetReply();
+GetInput();
+await GetReply();
+
+
+void GetInput() {
+    Console.Write("User: ");
+    string input = Console.ReadLine()!;
+    history.AddUserMessage(input);
+}
+
+async Task GetReply() {
+    ChatMessageContent reply = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel
+    );
+    Console.WriteLine("Assistant: " + reply.ToString());
+    history.AddAssistantMessage(reply.ToString());
+}
+
+void AddUserMessage(string msg) {
+    Console.WriteLine("User: " + msg);
+    history.AddUserMessage(msg);
+}
